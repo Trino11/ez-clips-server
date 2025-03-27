91e41c51df9c4c8e469d490e7295a32e4b6023fd
trino <rebollar.marquez.antoniomanuel@gmail.com>
2025-03-27 15:23:27 +0100

    Add initial project setup with README, configuration files, and basic server implementation

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js, prettier: prettierPlugin },
+    extends: ['js/recommended', prettier],
+    rules: {
+      'prettier/prettier': 'error', // Enforce Prettier rules as ESLint errors
+    },
+  },
+  tseslint.configs.recommended,
+]);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('Hello, world!');
+console.log(`.env working: ${process.env.MY_KEY}`);
